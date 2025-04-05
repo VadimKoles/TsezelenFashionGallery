a1bcc5034ac9f1b3fa9e2f7a8acc7c0d0735bdc5
Kolesnikov Vadim <vadimkolesnikov2101008@gmail.com>
2025-04-05 22:11:05 +1000

    correct marquee

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ function transformation(doTransformation, doTransition=true) {
         header.setAttribute('style', 'top: 27%; position: absolute; transition: height 0.5s, min-height 0.5s, background-color 1s; height: 44%; min-height: 270px; background-color: rgba(255, 255, 255, 0.72); flex-direction: column;')
         document.querySelector(".logo_full").src = "images/logo_tsezelen_full2_gradient_sqr.png"
         document.querySelector(".logo_full").setAttribute('style', 'height: 33dvh; min-height: 190px;')
-        document.querySelector(".marquee").style.display = "flex"
+        document.querySelector(".marquee").style.display = "block"
         document.querySelector(".logo_inscription").style.display = "none"
         document.querySelector(".burger_menu_icon").style.display = "none"
         doTransformStorage = false
